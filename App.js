d4402485959e7d253bf666385724d269bfb731fe
Rhine <790840451@qq.com>
2018-10-23 16:36:37 +0800

    feat: React Navigation 创建一个页面

--- App.js
+++ App.js
@@ -6,44 +6,36 @@
  * @flow
  */
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+// import React, {Component} from 'react'
+// import {Platform, StyleSheet, Text, View} from 'react-native'
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import { createStackNavigator } from 'react-navigation'
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+class HomeScreen extends React.Component {
+  // static navigationOptions = {
+  //   title: 'Welcome'
+  // }
+  render () {
+    return <Text>Hello, Navigation!</Text>
+  }
+}
 
-type Props = {};
-export default class App extends Component<Props> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{Platform.OS === 'ios' ? 'Press Cmd+R to reload' : 'Shake or press menu button for dev menu'}</Text>
-      </View>
-    );
+const SimpleApp = createStackNavigator({
+  Home: { screen: HomeScreen }
+})
+
+export default class App extends React.Component {
+  render () {
+    return <View style={styles.container}>
+      <SimpleApp />
+    </View>
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+    backgroundColor: '#F5FCFF'
+  }
+})
